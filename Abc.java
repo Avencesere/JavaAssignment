0847430c9b816f6a7a203a3c3140765bbd5678c0
Avencesere <113045259+Avencesere@users.noreply.github.com>
2022-09-23 09:22:04 +0530

    Day-3 Assignment

---
+++ Abc.java
@@ -0,0 +1,35 @@
+class Abc
+{
+
+
+   public static void main(String[] args)
+
+  {
+
+             int a=44;
+             int b=63;
+
+             System.out.println("The value of a before swapping is:" +a);
+             System.out.println("The value of b before swapping is:" +b +"\n--------------->>>>>>>>>>");
+
+             int c = a;
+
+             a=b;
+             c=b;
+             
+
+              
+
+              System.out.println("\n" + "The value of a after swapping is:" +a);
+              System.out.println("The value of b after swapping is:" +b);
+
+    
+ 
+       
+
+
+ }
+
+
+
+}
